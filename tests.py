80cd22bb1b2ded6882a25f182b1f793e1f086d8e
Maxwell Hunter <maxwell@treehousestickers.com>
2018-08-27 19:05:26 -0700

    Expanded Unit Test suite

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import unittest
 import bowling
 
 class TestGameMethods(unittest.TestCase):
+	#testing score of finished game method - new completed games
     def testPerfectGame(self):
 		self.assertEqual(bowling.Game('XXXXXXXXXXXX').score,300)
     def testNoStrikesGame(self):
@@ -17,5 +18,31 @@ class TestGameMethods(unittest.TestCase):
     def testRandomThreeGame(self):
 		self.assertEqual(bowling.Game('X7/90X088/06XXX81').score,167)
 
+    def testSpareBonuses(self):
+		self.assertEqual(bowling.add_spare_bonus(['5']),15)
+		self.assertEqual(bowling.add_spare_bonus(['X']),20)
+		self.assertEqual(bowling.is_strike('X'), True)
+
+    def testStrikeBonus(self):
+		self.assertEqual(bowling.add_strike_bonus(['2','7']),19)
+		self.assertEqual(bowling.add_strike_bonus(['X','7']),27)
+		self.assertEqual(bowling.add_strike_bonus(['4','X']),24)
+		self.assertEqual(bowling.add_strike_bonus(['X','X']),30)
+		self.assertEqual(bowling.add_strike_bonus(['4','/']),20)
+
+    def testIsStrike(self):
+		self.assertEqual(bowling.is_strike('X'), True)
+		self.assertEqual(bowling.is_strike('2'), False)
+		self.assertEqual(bowling.is_strike('/'), False)
+		self.assertEqual(bowling.is_spare('/'), True)
+		self.assertEqual(bowling.is_spare('5'), False)
+
+    def testResetPins(self):
+		self.assertEqual(bowling.reset_pins(), 10)
+		self.assertEqual(bowling.reset_pins('5'), 10)
+
+
 if __name__ == '__main__':
-	unittest.main()
+	suite = unittest.TestLoader().loadTestsFromTestCase(TestGameMethods)
+
+	unittest.TextTestRunner(verbosity=2).run(suite)
